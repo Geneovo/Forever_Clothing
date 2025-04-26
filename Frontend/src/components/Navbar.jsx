c1d67fc2c6385a0a56c7d0b2e5ea8509602ccd3f
Geneovo <bodjuvwederhie@gmail.com>
2025-04-26 09:10:06 +0100

    final commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
                 {token && isOpen &&
                 <div ref={dropdownRef} className='absolute right-0 pt-4 z-50'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black text-lg'>My Profile</p>
                         <p onClick={() => {
                             navigate('/orders')
                             setIsOpen(false);
