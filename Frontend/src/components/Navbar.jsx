5dd2920a7c6fdc35319e32c9586aa210f29eb844
Geneovo <bodjuvwederhie@gmail.com>
2025-04-26 08:51:33 +0100

    final commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
     const isCollectionPath = location.pathname.includes('collection');
 
     const [visible, setVisible] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
     const {setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
 
     const logout = () => {
@@ -52,14 +54,29 @@ const Navbar = () => {
             )}
 
             <div className='group relative'>
-                <img onClick={() => token ? null : navigate('/login')} src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+                <img onClick={() => {
+                    if (token) {
+                        setIsOpen(prev => !prev);
+                    } else {
+                        navigate('/login');
+                    }
+                 }} src={assets.profile_icon}className='w-5 cursor-pointer' alt="" />
                 {/* Dropdown Menu */}
-                {token &&
+                {token && isOpen &&
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
-                        <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
+                        <p onClick={() => {
+                            navigate('/orders')
+                            setIsOpen(false);
+                        }
+                        } 
+                        className='cursor-pointer hover:text-black'>Orders</p>
+                        <p onClick={() => {
+                            logout();
+                            setIsOpen(false);
+                        }} 
+                        className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
                 </div>}
             </div>
