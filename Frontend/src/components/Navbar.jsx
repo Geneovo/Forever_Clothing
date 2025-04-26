800a1d869a3cfe74b3c1369d90d8566b3a1efe26
Geneovo <bodjuvwederhie@gmail.com>
2025-04-26 09:03:03 +0100

    final commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
@@ -11,6 +11,22 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const {setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
+
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        function handleOutsideClick(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+    }, [])
 
     const logout = () => {
         navigate('/login');
@@ -53,7 +69,7 @@ const Navbar = () => {
                 </button>
             )}
 
-            <div className='group relative'>
+            <div className='relative'>
                 <img onClick={() => {
                     if (token) {
                         setIsOpen(prev => !prev);
@@ -63,7 +79,7 @@ const Navbar = () => {
                  }} src={assets.profile_icon}className='w-5 cursor-pointer' alt="" />
                 {/* Dropdown Menu */}
                 {token && isOpen &&
-                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div ref={dropdownRef} className='absolute right-0 pt-4 z-50'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
                         <p onClick={() => {
